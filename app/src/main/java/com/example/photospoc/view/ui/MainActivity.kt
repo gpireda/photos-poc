613e3a08aecc25c2409b91e9e68456f8ffb7b070
Gabriel Pireda <gabriel.pireda@gmail.com>
2020-07-19 16:50:57 -0300

    minor refactor

--- app/src/main/java/com/example/photospoc/view/ui/MainActivity.kt
+++ app/src/main/java/com/example/photospoc/view/ui/MainActivity.kt
@@ -31,14 +31,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun configureRecyclerView(photos: List<Photo>) {
         findViewById<RecyclerView>(R.id.photos_recycler_view).apply {
-            adapter = PhotosAdapter(photos) { item ->
-                val intent = PhotoDetailsActivity.getIntent(this@MainActivity)
-                intent.putExtra("PHOTO_DETAILS_INTENT", item.title)
-
-                startActivity(intent)
-            }
+            adapter = PhotosAdapter(photos) { handleItemClick(it) }
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
+    }
+
+    private fun handleItemClick(item: Photo) {
+        val intent = PhotoDetailsActivity.getIntent(this@MainActivity)
+        intent.putExtra("PHOTO_DETAILS_INTENT", item.title)
+
+        startActivity(intent)
     }
 }
